3128164d73741cb41fce629924a148a842349952
Rana Soyak <rselvasoyak@gmail.com>
2023-02-07 16:03:51 -0500

    saved remaining route changes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,14 @@
-const Header = () => {
+import { Link } from 'react-router-dom';
+
+const HomePage = () => {
     return (
         <section className="homePage">
             <h1> Leaf-by-Leaf <span className="homeHeader">Let's Become</span> <span className="homeHeader">Better Plant Parents</span></h1>
-            <button className="changePage">Let's Do It!</button>
+            <Link to="/contentPage">
+                <button className="changePage">Let's Do It!</button>
+            </Link>
         </section>
     )
 }
 
-export default Header;
+export default HomePage;
